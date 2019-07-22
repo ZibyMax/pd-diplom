ee861371b1460e7489a2857c3455c963c431aacc
ZibyMax <max@rekstar-nn.ru>
2019-07-22 18:30:23 +0300

    pause

--- getgoods/import_data.py
+++ getgoods/import_data.py
@@ -21,21 +21,16 @@ def main():
     sys.path.append(os.path.join(BASE_DIR, 'getgoods'))
     os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
     django.setup()
+    from orders.models import Shop, Category, Product, ProductInfo, Parameter,\
+        ProductParameter, Order, OrderItem, Contact
 
     file_list = os.listdir(os.path.join(BASE_DIR, 'data'))
     for file in file_list:
         data = read_yaml(os.path.join(BASE_DIR, 'data', file))
-        # pprint(data)
+        pprint(data)
 
-    models = list(filter(lambda model: model.__module__ == 'orders.models', django.apps.apps.get_models()))
-    shop = None
-    for model in models:
-        if model.__name__ == 'Shop':
-            shop = model
-    print(shop)
-    s1 = shop(name='Megafon')
-    print(s1)
-    s1.save()
+    obj, created = Shop.objects.update_or_create(id=1)
+    print(obj)
 
 
 if __name__ == '__main__':
